a70a88ed40bbb01cd7194394b6760e3c9b49292f
quochung0909 <1851010054hung@ou.edu.vn>
2023-04-29 21:53:25 +0700

    update FibonacciLastDigit

--- Lab11/FibonacciLastDigit.java
+++ Lab11/FibonacciLastDigit.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
-        if (n <= 1)
+        if (n <= 1) {
             return n;
-
-        int previous = 0;
-        int current  = 1;
-
-        for (int i = 0; i < n - 1; ++i) {
-            int tmp_previous = previous;
-            previous = current;
-            current = tmp_previous + current;
         }
-        System.out.println("current = " + current);
-
-        return current % 10;
+    
+        int previous = 0;
+        int current = 1;
+    
+        for (int i = 2; i <= n; i++) {
+            int next = (previous + current) % 10;
+            previous = current;
+            current = next;
+        }
+    
+        return current;
     }
     
     public static void main(String[] args) {
